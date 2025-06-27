40cdb7f6f7ac18820c30b058528cc8432b65e0d4
Noah Stevens <nstev009@uottawa.ca>
2025-06-27 17:16:04 -0400

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/nstev009.github.io/' // For GitHub Pages compatibility
-})
